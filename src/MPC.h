3639cf005a915f5925bb6efe6156cbd9cddba65c
Artur Olszak <a.olszak@polbyte.com>
2017-06-29 12:01:35 +0200

    + MPC implemented.

--- src/MPC.h
+++ src/MPC.h
@@ -6,15 +6,27 @@
 
 using namespace std;
 
+const int latency_multiplier = 2; // 100ms latency which equals 2*dt
+
+struct SolveVector {
+  vector<double> x; // position x
+  vector<double> y; // position y
+  vector<double> steering; // steering value
+  vector<double> throttle; // speed value <-1,1>
+};
+
 class MPC {
  public:
+  
   MPC();
-
   virtual ~MPC();
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  SolveVector Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  
+  double previous_steering {0};
+  double previous_throttle {0};
 };
 
 #endif /* MPC_H */
